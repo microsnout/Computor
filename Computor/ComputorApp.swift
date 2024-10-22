c93f8f5394aa529e00762193c94c6134040044ed
Barry Hall <microsnout@gmail.com>
2024-10-22 06:17:23 -0300

    Initial Commit

---
+++ Computor/ComputorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputorApp.swift
+//  Computor
+//
+//  Created by Barry Hall on 2024-10-22.
+//
+
+import SwiftUI
+
+@main
+struct ComputorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComputorTests/ComputorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComputorTests.swift
+//  ComputorTests
+//
+//  Created by Barry Hall on 2024-10-22.
+//
+
+import Testing
+@testable import Computor
+
+struct ComputorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
