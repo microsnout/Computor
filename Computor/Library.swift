aa200f301b54497ba68de93074b3bfea8e9a85ee
microsnout <microsnout@gmail.com>
2025-06-09 08:30:08 -0300

    adding new file stuff

--- Computor/Library.swift
+++ Computor/Library.swift
@@ -27,3 +27,38 @@ struct MacroLibrary : Codable {
     // Definitions of Fn programmable keys
     var macroList: [SymbolTag : MacroRec] = [:]
 }
+
+
+// New code, not yet in service
+
+typealias GroupId = Int
+
+struct MacroRecTable: Codable {
+    
+    /// One of these files per macro lib group
+    
+    var id:         UUID
+    var caption:    String?
+    var groupTable: [UUID]
+    var macroTable: [MacroRec]
+}
+
+
+struct MacroFileRec: Codable {
+    
+    /// Description of one macro library file
+    /// Contains a list of all symbols defined in file
+    
+    var id: UUID
+    var caption: String?
+    var symList: [SymbolTag]
+}
+
+
+struct MacroFileTable: Codable {
+    
+    /// Only one of these tables per app
+    /// Contains a record of each macro library file
+    
+    var fileTable:  [MacroFileRec]
+}
