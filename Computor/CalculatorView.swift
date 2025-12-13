deb47869a7f65bc9b3440c3e41f1fff6dfc573fb
microsnout <microsnout@gmail.com>
2025-12-13 08:01:51 -0400

    loading view

--- Computor/CalculatorView.swift
+++ Computor/CalculatorView.swift
@@ -19,28 +19,37 @@ struct CalculatorView: View {
     
     @State private var timer: Timer?
     
+    @State private var initializer = AppInitializer()
     
     var body: some View {
         
         Group {
-            if hSizeClass == .regular && vSizeClass == .compact {
-                LandscapeView( model: model )
+            switch initializer.state {
+                
+            case .loading:
+                LoadingView( msg: "Loading...")
+                
+            case .loaded:
+                Group {
+                    if hSizeClass == .regular && vSizeClass == .compact {
+                        LandscapeView( model: model )
+                    }
+                    else {
+                        PortraitView( model: model )
+                    }
+                }
+                
+            case .failed( let error ):
+                LoadingView( msg: String( describing: error ) )
             }
-            else {
-                PortraitView( model: model )
-            }
+            
+            
         }
         .environment(model)
-//        .task {
-//            // Load calculator documents and macro modules
-//            model.db.loadDatabase()
-//            
-//            // Activate doc0
-//            model.loadModule( modZeroSym )
-//            
-//            // Set aux display view to mod zero
-//            model.aux.macroMod = model.db.getModuleFileRec(sym: modZeroSym) ?? ModuleRec( name: "?")
-//        }
+        .task {
+            // This task runs the async initialization when the view appears
+            await initializer.initialize(model)
+        }
         .onAppear() {
             // Create regular timer pulse - 1 sec
             timer = Timer.scheduledTimer( withTimeInterval: Const.Model.clockTick, repeats: true) { _ in
@@ -60,6 +69,40 @@ struct CalculatorView: View {
 }
 
 
+@MainActor
+@Observable
+class AppInitializer {
+    
+    enum State {
+        case loading
+        case loaded
+        case failed(Error)
+    }
+    
+    var state: State = .loading
+    
+    func initialize( _ model: CalculatorModel ) async {
+        do {
+            // Load calculator documents and macro modules
+            model.db.loadDatabase()
+
+            // Activate doc0
+            model.loadModule( modZeroSym )
+
+            // Set aux display view to mod zero
+            model.aux.macroMod = model.db.getModuleFileRec(sym: modZeroSym) ?? ModuleRec( name: "?")
+            
+            // Simulate an asynchronous initialization task, e.g., network request, database setup
+            // try await Task.sleep( for: .seconds(2))
+            state = .loaded
+        }
+        catch {
+            state = .failed(error)
+        }
+    }
+}
+
+
 struct PortraitView : View {
     
     @State var model: CalculatorModel
@@ -68,6 +111,48 @@ struct PortraitView : View {
 
     var body: some View {
         
+        ZStack {
+            PortraitBackground()
+            
+            KeyStack( keyPressHandler: model ) {
+                VStack( spacing: 5 ) {
+                    VStack( spacing: 0 ) {
+                        
+                        // Auxiliary Display
+                        AuxDisplayGroup( model: model )
+                        
+                        // Computor title and settings control gear
+                        TitleBar( model: model, presentSettings: $presentSettings )
+                    }
+                    .padding(0)
+                    
+                    // Main calculator display
+                    DisplayView( model: model )
+                    Spacer().frame( height: 3)
+
+                    // Keypads
+                    KeypadGroup( model: model )
+                    Spacer()
+                }
+                .padding(.horizontal, 30)
+                .padding(.vertical, 5)
+                .background( Color("Background"))
+            }
+            .ignoresSafeArea(.keyboard)
+        }
+        .sheet( isPresented: $presentSettings ) {
+            ControlView( model: model )
+                .presentationDetents( [.fraction(0.7), .fraction(1.0)] )
+        }
+    }
+}
+
+
+struct LoadingView : View {
+    
+    var msg: String
+    
+    var body: some View {
         ZStack {
             Color( Color("SafeBack") )
                 .edgesIgnoringSafeArea( .all )
@@ -82,94 +167,136 @@ struct PortraitView : View {
                         .padding( [.leading, .trailing], 15 )
                 )
             
-            KeyStack( keyPressHandler: model ) {
-                VStack( spacing: 5 ) {
-                    VStack( spacing: 0 ) {
-                        AuxiliaryDisplayView( model: model, auxView: $model.aux.activeView )
-                        
-                        DotIndicatorView( currentView: $model.aux.activeView )
-                            .padding( .top, 5 )
-                            .frame( maxHeight: 8)
-                        
-                        HStack {
-                            let modName = model.activeModName == modZeroSym ? "" : model.activeModName
-                            
-                            Text(Const.Str.appName).foregroundColor(Color("Frame"))/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/().italic()
-                            
-                            Spacer()
-                            
-                            RichText( modName, size: .small, weight: .bold, design: .default, defaultColor: "AccentText" )
-                            Button {
-                                presentSettings = true
-                            } label: {
-                                Image( systemName: "gearshape").foregroundColor(Color("Frame"))
-                            }
-                        }
-                        .frame( maxHeight: 22 )
-                    }
-                    .padding(0)
-                    
-                    // Main calculator display
-                    DisplayView( model: model )
-                    
-                    Spacer().frame( height: 3)
-                    
-                    VStack( spacing: 7 ) {
-                        HStack {
-                            KeypadView( padSpec: psSoftkeyL, keyPressHandler: model )
-                            Spacer()
-                            KeypadView( padSpec: psSoftkeyR, keyPressHandler: model )
-                        }
-                        HStack {
-                            KeypadView( padSpec: psUnitsL, keyPressHandler: model )
-                            Spacer()
-                            KeypadView( padSpec: psUnitsR, keyPressHandler: model )
-                        }
-                        HStack {
-                            KeypadView( padSpec: psFunctions2L, keyPressHandler: model )
-                            Spacer()
-                            KeypadView( padSpec: model.kstate.func2R, keyPressHandler: model )
-                        }
-                        HStack {
-                            KeypadView( padSpec: psFunctionsL, keyPressHandler: model )
-                            Spacer()
-                            KeypadView( padSpec: psFunctionsR, keyPressHandler: model )
-                        }
-                    }
-                    
-                    VStack( spacing: 5) {
-                        Divider()
-                        HStack {
-                            VStack( spacing: 10 ) {
-                                KeypadView( padSpec: psNumeric, keyPressHandler: model )
-                                KeypadView( padSpec: psEnter, keyPressHandler: model )
-                            }
-                            Spacer()
-                            VStack( spacing: 10 ) {
-                                KeypadView( padSpec: psOperations, keyPressHandler: model )
-                                KeypadView( padSpec: psClear, keyPressHandler: model )
-                            }
-                        }
-                    }
-                    VStack( spacing: 5 ) {
-                        Divider().frame(height: 1)
-                        HStack {
-                            KeypadView( padSpec: psFormatL, keyPressHandler: model )
-                            Spacer()
-                            KeypadView( padSpec: psFormatR, keyPressHandler: model )
-                        }
-                    }
-                    Spacer()
-                }
-                .padding(.horizontal, 30)
-                .padding(.vertical, 5)
-                .background( Color("Background"))
+            VStack( spacing: 0 ) {
+                Text(msg)
             }
+            .padding(.horizontal, 30)
+            .padding(.vertical, 5)
+            .background( Color("Background"))
             .ignoresSafeArea(.keyboard)
         }
-        .sheet( isPresented: $presentSettings ) {
-            ControlView( model: model )
-                .presentationDetents( [.fraction(0.7), .fraction(1.0)] )
+    }
+}
+
+
+struct PortraitBackground: View {
+    
+    var body: some View {
+        
+        Group {
+            Color( Color("SafeBack") )
+                .edgesIgnoringSafeArea( .all )
+            
+            Rectangle()
+                .fill(Color("Background"))
+                .cornerRadius(15)
+                .padding( [.leading, .trailing], 15 )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.black, lineWidth: 1)
+                        .padding( [.leading, .trailing], 15 )
+                )
+        }
+    }
+}
+
+
+struct AuxDisplayGroup: View {
+    
+    @State var model: CalculatorModel
+    
+    var body: some View {
+        
+        Group {
+            AuxiliaryDisplayView( model: model, auxView: $model.aux.activeView )
+            
+            DotIndicatorView( currentView: $model.aux.activeView )
+                .padding( .top, 5 )
+                .frame( maxHeight: 8)
+        }
+    }
+}
+
+
+struct TitleBar: View {
+    
+    var model: CalculatorModel
+
+    @Binding var presentSettings: Bool
+
+    var body: some View {
+        
+        HStack {
+            let modName = model.activeModName == modZeroSym ? "" : model.activeModName
+            
+            Text(Const.Str.appName).foregroundColor(Color("Frame"))/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/().italic()
+            
+            Spacer()
+            
+            RichText( modName, size: .small, weight: .bold, design: .default, defaultColor: "AccentText" )
+            Button {
+                presentSettings = true
+            } label: {
+                Image( systemName: "gearshape").foregroundColor(Color("Frame"))
+            }
+        }
+        .frame( maxHeight: 22 )
+    }
+}
+
+
+struct KeypadGroup: View {
+    
+    var model: CalculatorModel
+    
+    var body: some View {
+        
+        Group {
+            VStack( spacing: 7 ) {
+                HStack {
+                    KeypadView( padSpec: psSoftkeyL, keyPressHandler: model )
+                    Spacer()
+                    KeypadView( padSpec: psSoftkeyR, keyPressHandler: model )
+                }
+                HStack {
+                    KeypadView( padSpec: psUnitsL, keyPressHandler: model )
+                    Spacer()
+                    KeypadView( padSpec: psUnitsR, keyPressHandler: model )
+                }
+                HStack {
+                    KeypadView( padSpec: psFunctions2L, keyPressHandler: model )
+                    Spacer()
+                    KeypadView( padSpec: model.kstate.func2R, keyPressHandler: model )
+                }
+                HStack {
+                    KeypadView( padSpec: psFunctionsL, keyPressHandler: model )
+                    Spacer()
+                    KeypadView( padSpec: psFunctionsR, keyPressHandler: model )
+                }
+            }
+            
+            VStack( spacing: 5) {
+                Divider()
+                HStack {
+                    VStack( spacing: 10 ) {
+                        KeypadView( padSpec: psNumeric, keyPressHandler: model )
+                        KeypadView( padSpec: psEnter, keyPressHandler: model )
+                    }
+                    Spacer()
+                    VStack( spacing: 10 ) {
+                        KeypadView( padSpec: psOperations, keyPressHandler: model )
+                        KeypadView( padSpec: psClear, keyPressHandler: model )
+                    }
+                }
+            }
+            VStack( spacing: 5 ) {
+                Divider().frame(height: 1)
+                HStack {
+                    KeypadView( padSpec: psFormatL, keyPressHandler: model )
+                    Spacer()
+                    KeypadView( padSpec: psFormatR, keyPressHandler: model )
+                }
+            }
         }
     }
 }
