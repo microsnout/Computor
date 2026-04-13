1acc56663ce72ebc18a848bfbfb4541d841efbe1
microsnout <microsnout@gmail.com>
2026-04-13 10:21:21 -0300

    Adding base class BaseRec

---
+++ Computor/Aux/AuxItemList.swift
@@ -0,0 +1,18 @@
+//
+//  AuxItemList.swift
+//  Computor
+//
+//  Created by Barry Hall on 2026-04-12.
+//
+
+import SwiftUI
+
+struct AuxItemList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuxItemList()
+}
